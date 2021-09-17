7c257fa69b459ea6c9a0fce846e849f1f9ddf8a1
Seikyoo <joe.montoya@outlook.fr>
2021-09-17 20:53:54 +0200

    feat: added style on submit button

--- view/articles/new.php
+++ view/articles/new.php
@@ -1,13 +1,13 @@
 <h1>Nouvel article</h1>
 
-<div class="wrapper">
-    <form action="" method="post">
-        <label for="title">
-            <input type="text" name="title" class="form-input" value="<?= $_POST['title'] ?? '' ?>" placeholder="Titre de votre article">
-        </label>
-        <label for="content">
-            <textarea name="content" class="form-textarea" placeholder="Contenu de l'article"><?= $_POST['content'] ?? '' ?></textarea>
-        </label>
-        <button type="submit">Envoyer</button>
-    </form>
-</div>
+<form action="" method="post">
+    <label for="title">
+        <input type="text" name="title" class="form-input" value="<?= $_POST['title'] ?? '' ?>"
+               placeholder="Titre de votre article">
+    </label>
+    <label for="content">
+        <textarea name="content" class="form-textarea"
+                  placeholder="Contenu de l'article"><?= $_POST['content'] ?? '' ?></textarea>
+    </label>
+    <button type="submit" class="form-submit">Enregistrer</button>
+</form>
